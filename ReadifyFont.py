3d33075352868eb934ddd7e60cb7a948a0a8c0fa
Sherman Perry <shermp@users.noreply.github.com>
2015-10-18 16:13:55 +1300

    Initial commit

---
+++ ReadifyFont.py
@@ -0,0 +1,185 @@
+﻿from ReadifyFrame import *
+import os
+import subprocess
+import sys
+
+MISSING_VALUE = 1
+INVALID_VALUE = 2
+PROC_OS_ERROR = 100
+PROC_SUCCESS = 0
+
+SEL_REGULAR = 0
+SEL_ITALIC = 1
+SEL_BOLD = 2
+SEL_BOLDITALIC = 3
+SEL_NONE = 4
+
+class ReadifyFontGUI(ReadifyFrame):
+    """
+    This class extends the ReadifyFrame class. It shows the GUI described in the parent class, and handles the events to
+    choose fonts, and generate new font files.
+    """
+    fontDir = None
+    def __init__(self):
+        super(ReadifyFontGUI, self).__init__(None)
+        ReadifyFontGUI.Show(self)
+
+    def chooseFiles(self, event):
+        """
+        Browse for font files using default file dialog box. Font files are added to the wxTextCtrl,
+        and the subfamily of font is automatically determined if possible.
+        :param event:
+        :return:
+        """
+        dialog = wx.FileDialog(self, "Choose a file", "", "", "*.otf", wx.MULTIPLE)
+        if dialog.ShowModal() == wx.ID_OK:
+            fileList = dialog.GetFilenames()
+            self.fontDir = os.path.dirname(dialog.GetPath())
+            # Try to determine font subfamily and set the listChoiceStyle to the appropriate value
+            for file, text, style in zip(fileList, self.listTxtFont, self.listChoiceStyle):
+                text.SetValue(file)
+                if "regular" in file.lower():
+                    style.SetSelection(SEL_REGULAR)
+                elif "bold" in file.lower() and "italic" in file.lower():
+                    style.SetSelection(SEL_BOLDITALIC)
+                elif "bold" in file.lower():
+                    style.SetSelection(SEL_BOLD)
+                elif "italic" in file.lower():
+                    style.SetSelection(SEL_ITALIC)
+                else:
+                    style.SetSelection(SEL_NONE)
+            for txtBox in self.listTxtFont:
+                if txtBox.GetValue:
+                    txtBox.Disable()
+                    
+    def genTTF(self, event):
+        """
+        Generate the new font files. Builds a list of command line arguments from the options in the GUI and passes
+        the list to the runCLI() function. A status message is displayed, and the output of the runCLI() function is
+        displayed in the output text box.
+        :param event:
+        :return:
+        """
+
+        # Create a dictionary of all possible command line arguments, and set all values to None
+        optsDic = {"fontname" : None, "regular" : None, "italic" : None, "bold" : None, "bolditalic" : None, "striphint" : None, \
+            "legacykern" : None, "outputdir" : None, "addweight" : None, "panosestrip" : None, "modbearings" : None, "namehack" : None}
+
+        # If a font file exits, set its option in the dictionary
+        for font, style in zip(self.listTxtFont, self.listChoiceStyle):
+            if font.GetValue() and style.GetSelection() == 0:
+                optsDic["regular"] = "-r " + os.path.join(self.fontDir, font.GetValue())
+            elif font.GetValue() and style.GetSelection() == 1:
+                optsDic["italic"] = "-i " + os.path.join(self.fontDir, font.GetValue())
+            elif font.GetValue() and style.GetSelection() == 2:
+                optsDic["bold"] = "-b " + os.path.join(self.fontDir, font.GetValue())
+            elif font.GetValue() and style.GetSelection() == 3:
+                optsDic["bolditalic"] = "-B " + os.path.join(self.fontDir, font.GetValue())
+
+        # Set the new font name in the dictionary
+        if self.txtFontFamName.GetValue():
+            optsDic["fontname"] = self.txtFontFamName.GetValue()
+
+        # Check that a font file and name have been selected by the user
+        if not any((optsDic["regular"], optsDic["italic"], optsDic["bold"], optsDic["bolditalic"])):
+            wx.MessageBox("You need to choose at least One font")
+            return MISSING_VALUE
+
+        if not optsDic["fontname"]:
+            wx.MessageBox("You need to choose a new font name!")
+            return MISSING_VALUE
+
+        # Add more options to the dictionary
+        if self.chkHint.GetValue():
+            optsDic["striphint"] = "-s"
+        if self.chkKern.GetValue():
+            optsDic["legacykern"] = "-k"
+        if self.chkPANOSE.GetValue():
+            optsDic["panosestrip"] = "-p"
+        if self.chkAltName.GetValue():
+            optsDic["namehack"] = "-n"
+        if self.txtDarkenAmount.GetValue():
+            # lets check that the user enters a valid number
+            try:
+                intWeight = int(self.txtDarkenAmount.GetValue())
+                if 1 <= intWeight <= 100:
+                    optsDic["addweight"] = "-w " + self.txtDarkenAmount.GetValue()
+                    if self.chkBearing.GetValue():
+                        optsDic["modbearings"] = "-m"
+                else:
+                    wx.MessageBox("Darken amount too high or low! Please choose a number between 1 and 100 em.")
+                    return INVALID_VALUE
+            except ValueError:
+                wx.MessageBox("You did not enter a valid number! Your font will not be darkened.")
+                return INVALID_VALUE
+
+        # Choose an output directory for the generated fonts
+        if self.pickerOutDir.GetPath():
+            optsDic["outputdir"] = "-d " + self.pickerOutDir.GetPath()
+        else:
+            optsDic["outputdir"] = "-d " + self.fontDir
+
+        # Create a list of command line arguments to pass to subprocess
+        optionsList = ["fontforge", "-script", "ReadifyFontCLI.py"]
+        for key, val in optsDic.iteritems():
+            if val and key != "fontname":
+                optionsList.append(val)
+        optionsList.append(optsDic["fontname"])
+
+        # Run script, and obtain its outputs
+        cliRetCode, cliOutput = self.runCLI(optionsList)
+        strCliOutput = cliOutput.decode(sys.stdout.encoding)
+        # Check for success
+        if cliRetCode == PROC_OS_ERROR:
+            wx.MessageBox("Oops, something went wrong.\nIs FontForge installed and in your PATH?")
+        elif cliRetCode != PROC_SUCCESS:
+            self.txtOutput.SetValue(strCliOutput)
+            wx.MessageBox("The font was not generated. Please see log for details")
+        else:
+            self.txtOutput.SetValue(strCliOutput)
+            wx.MessageBox("The font was successfully generated!")
+
+
+    def runCLI(self, optList):
+        """
+        Uses the subprocess.check_output() function to run a list of commands line arguments.
+        :param optList:
+        :return:
+        """
+        try:
+            rfCLI = subprocess.check_output(optList, stderr=subprocess.STDOUT)
+
+            return PROC_SUCCESS, rfCLI
+        # In case fontforge is not in PATH, or some other OS related error
+        except OSError:
+            return PROC_OS_ERROR, None
+        # Check if fontforge script completed successfully or not
+        except subprocess.CalledProcessError as scriptFailed:
+            retCode = scriptFailed.returncode
+            output = scriptFailed.output
+            return retCode, output
+
+    def clearForm(self, event):
+        """
+        Resets/clears all of the elements in the GUI to a clean/new state.
+        :param event:
+        :return:
+        """
+        for text, style in zip(self.listTxtFont, self.listChoiceStyle):
+            text.SetValue(wx.EmptyString)
+            text.Enable()
+            style.SetSelection(SEL_NONE)
+        self.txtFontFamName.SetValue(wx.EmptyString)
+        self.chkAltName.SetValue(False)
+        self.chkBearing.SetValue(False)
+        self.chkHint.SetValue(False)
+        self.chkKern.SetValue(False)
+        self.chkPANOSE.SetValue(False)
+        self.pickerOutDir.SetPath(wx.EmptyString)
+        self.txtDarkenAmount.SetValue(wx.EmptyString)
+        self.txtOutput.SetValue(wx.EmptyString)
+
+if __name__ == "__main__":
+    app = wx.App(False)
+    frame = ReadifyFontGUI()
+    app.MainLoop()
---
+++ ReadifyFontCLI.py
@@ -0,0 +1,190 @@
+﻿# This script should be called using '$ fontforge -script ReadifyFontCLI.py [OPTIONS] FONTNAME'
+#
+# Readify Font by Sherman Perry
+# 
+# Originaly distributed on Mobileread forums
+#
+# Feel free to modify and redistribute this script
+# Please consider sending me modifications or an improved script at mobileread.com
+# My mobileread username is 'sherman'
+#
+# Current Version: Readify Font 0.1
+# It has plenty of rough edges...
+
+import os
+import sys
+import argparse
+import fontforge
+
+FNT_REGULAR = 'Regular'
+FNT_ITALIC = 'Italic'
+FNT_BOLD = 'Bold'
+FNT_BOLD_ITALIC = 'BoldItalic'
+
+def changeWeight(glyph, emboldenAmount, modifyBearings):
+    origWidth = glyph.width
+    glyph.changeWeight(emboldenAmount)
+    
+    '''
+    Change the bearings so that the effective font weight is approximately equal before and after the
+    change weight operation.
+    
+    There has been stability issues observed with the following code (Windows 10 x64). 
+    Seems to have happened more with longer output file paths
+    Checking if bearingChange>0 seems to have helped. I have not determined the root cause of this, 
+    as the python interpreter crashes without providing a traceback
+    This stability issue may be Windows related. Or (more likely) I've made a big mistake somewhere...
+    '''
+    if modifyBearings:
+        newWidth = glyph.width
+        widthChange = newWidth - origWidth
+        bearingChange = widthChange/2
+        
+        # Try and make this more stable
+        if bearingChange > 0:
+            glyph.left_side_bearing -= bearingChange
+            glyph.right_side_bearing -= bearingChange
+
+def modLayer(glyph):
+    # Get active layer object
+    lay = glyph.layers[glyph.activeLayer]
+    
+    # get iterator of contours
+    contours = lay.__iter__()
+    
+    # for any open contours, close them
+    for c in contours:
+        if not c.closed:
+            c.closed=True
+    
+    # Assign the modified layer object back to the glyph
+    glyph.layers[glyph.activeLayer] = lay
+    glyph.correctDirection()
+    
+def generateFlags(stripHints, legacyKern):
+    if stripHints and legacyKern:
+        flags = ('opentype', 'old-kern', 'round', 'no-hints')
+    elif stripHints and not legacyKern:
+        flags = ('opentype', 'round', 'no-hints')
+    elif legacyKern and not stripHints:
+        flags = ('opentype', 'old-kern', 'round')
+    else:
+        flags = ('opentype', 'round')
+    return flags
+
+    
+
+def setNames(font, family, subfamily):
+    # PostScript font names
+    fontName = family.replace(' ', '')+'-'+subfamily
+    fullName = family + ' ' + subfamily
+    # Set PS names
+    font.fontname = fontName
+    font.familyname = family
+    font.fullname = fullName
+    # Set SFNT names
+    font.appendSFNTName('English (US)', 'Family', unicode(family, 'utf-8'))
+    if subfamily == FNT_BOLD_ITALIC:
+        font.appendSFNTName('English (US)', 'SubFamily', unicode('Bold Italic', 'utf-8'))
+    else:
+        font.appendSFNTName('English (US)', 'SubFamily', unicode(subfamily, 'utf-8'))
+        font.appendSFNTName('English (US)', 'Fullname', unicode(fullName, 'utf-8'))
+
+parser = argparse.ArgumentParser()
+parser.add_argument('fontname', help='Specify new font name')
+parser.add_argument('-r', '--regular', help='Regular font file')
+parser.add_argument('-i', '--italic' , help='Italic font file')
+parser.add_argument('-b', '--bold' , help='bold font file')
+parser.add_argument('-B', '--bolditalic' , help='Bold Italic font file')
+parser.add_argument('-s', '--striphint' , help='Strip hints from font', action="store_true")
+parser.add_argument('-k', '--legacykern' , help='Include legacy kerning table', action="store_true")
+parser.add_argument('-d', '--outputdirectory' , help='Output directory if set. Default is "./kobomodified/"')
+parser.add_argument('-w', '--addweight' , help='Add weight to font. Values around 8-15 seems suitable. 50 is bold', type=int)
+parser.add_argument('-p', '--panosestrip' , help='Strip PANOSE data from font', action="store_true")
+parser.add_argument('-m', '--modifybearings' , help='Modify bearings when adding weight. This has no affect when not adding weight. \
+                    Only use it for subtle weight changes', action="store_true")
+parser.add_argument('-n', '--namehack' , help='If the fonts generated have internal names different to what you specified, \
+                    try this option to enable an ugly workaround. It basically generates the font twice.', action="store_true")
+args = parser.parse_args()
+
+if not any((args.regular, args.italic, args.bold, args.bolditalic)):
+    raw_input('At least one font must be specified. Press ENTER to exit...')
+    sys.exit()
+
+fontDic = {FNT_REGULAR : args.regular, FNT_ITALIC : args.italic, FNT_BOLD : args.bold, FNT_BOLD_ITALIC : args.bolditalic}
+
+userSuppliedPath = args.outputdirectory
+
+if userSuppliedPath:
+    outDir = userSuppliedPath.strip() + '/'
+else:
+    outDir = './koboified/'
+
+outDir = os.path.normpath(outDir)
+print(outDir)
+try: 
+    os.makedirs(outDir)
+except OSError:
+    if not os.path.isdir(outDir):
+        raise
+
+newFamilyName = args.fontname.strip()
+stripHints = args.striphint
+legacyKern = args.legacykern
+addWeight = args.addweight
+
+
+for style, fontFile in fontDic.iteritems():
+    # Check if a font has been added before proceeding
+    if fontFile:
+        # Open font file and immediately save as a fontforge file
+        f = fontforge.open(fontFile.strip())
+        newFontFile = os.path.normpath(outDir+'/'+newFamilyName+'-'+style+'.sfd')
+        newFontTTF = os.path.normpath(outDir+'/'+newFamilyName+'-'+style+'.ttf')
+        
+        f.save(newFontFile)
+        f.close()
+        
+        # Open new fontforge file
+        f = fontforge.open(newFontFile)
+        
+        # Set the font names in the SFNT names table
+        setNames(f, newFamilyName, style)
+        
+        # Replace PANOSE Data with 'Any', or 0
+        if args.panosestrip:
+            f.os2_panose = (0,0,0,0,0,0,0,0,0,0)
+                
+        # Iterate over all glyphs in font, and darken regular and italic fonts only
+        allGlyphs=f.glyphs()
+        for glyph in allGlyphs:
+            if style in (FNT_REGULAR, FNT_ITALIC) and addWeight:
+                changeWeight(glyph, addWeight, args.modifybearings)
+            # Make some modifications to better suit truetype outlines
+            modLayer(glyph)
+        
+        # If I've understood things correctly, this should be the same as setting the curves in the
+        # font information screen of the GUI
+        for l in range(0, f.layer_cnt):
+            if not f.layers[l].is_quadratic:
+                f.layers[l].is_quadratic = True
+                print('The curves in the '+f.layers[l].name+' layer were converted to be quadratic')
+        
+        
+        print('\nSaving '+newFontTTF+'. . .\n')
+
+        flagsTTF = generateFlags(stripHints, legacyKern)
+        f.generate(newFontTTF, flags=flagsTTF)
+        
+        f.save(newFontFile)
+        f.close()
+        ''' 
+        This is an ugly workaround to the font renaming issue some fonts seem to have
+        One day, I hope to find a proper fix, or a more elegent workaround
+        '''
+        if args.namehack:
+            f = fontforge.open(newFontTTF)
+            setNames(f, newFamilyName, style)
+            f.generate(newFontTTF, flags=flagsTTF)
+            f.close()
+
